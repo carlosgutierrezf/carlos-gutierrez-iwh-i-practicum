ccd92943e04fac022c9bb023594bc930112ab739
HubSpotSupport <andres.diaz@onthefuze.com.au>
2024-06-04 16:48:03 -0500

    Second commit

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-const private_app_token = 'pat-na1-2bbf146e-d713-4c9f-b50f-f9ae2eabf358';
-
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = '';
